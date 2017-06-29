93d3b7363fc9141995dddfb123cda75850cc4c17
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-06-28 21:02:22 -0400

    first commit added html and css styling to page

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+
+
+});
+
+// Example from class!
+	// <form id="my-form">
+	// 	First name:<br>
+	// 	<input type="text" id="first-name">
+	// 	<input type="submit" value="Submit">
+	// </form>
+
+	// <script src="cookies.js" type="text/javascript"></script>
+
+	// <script>
+
+	// 	var x = (Cookies.get("firstName"));
+
+	// 	if (x) {
+	// 		alert("hi " + x);
+	// 	} else {
+	// 		alert("enter your name");
+	// 	}
+	
+
+	// 	$("#my-form").submit(function(event) {
+	// 		event.preventDefault(); 
+	// 		// have to prevent the default value of submit to try to send to server and refresh page
+
+	// 		var name = $("#first-name").val();
+	// 		Cookies.set("firstName", name);
+	// 	});	
+	// </script>
+
