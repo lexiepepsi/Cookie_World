d3871d314ce4d93e858aa9ffb37e977b730039c3
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-07-03 01:32:39 -0400

    added initial js code to set and get cookies

--- main.js
+++ main.js
@@ -1,7 +1,57 @@
 $(document).ready(function() {
-	$("cookieimg").hover() {
-		alert("Enter the number");
-	};
+
+	if (Cookies.get("choc-key")) {
+		alert("You'd like " + Cookies.get("choc-key") + " chocolate cookies");
+	}
+
+	// ^this works bc undefined = false, so if Cookies.get("choc-key") returns undefined, undefined will be false, if there is any value the conditional will be true and the code will run and create an alert
+
+	if (Cookies.get("sugar-key")) {
+		alert("You'd like " + Cookies.get("sugar-key") + " sugar cookies");
+	}
+
+	if (Cookies.get("lemon-key")) {
+		alert("You'd like " + Cookies.get("lemon-key") + " lemon cookies");
+	}
+
+
+
+	$("#choc-form").submit(function(event) {
+		event.preventDefault();
+
+		var numberofchocs = $("#choc-input").val();
+		Cookies.set("choc-key", numberofchocs);
+
+		// the value of choc-input should be stored in a variable called numberchoc
+		// then set a cookie with the value choc-key = (numberchoc)
+
+	});
+
+
+	$("#sugar-form").submit(function(event) {
+		event.preventDefault();
+
+		var numberofsugars = $("#sugar-input").val();
+		Cookies.set("sugar-key", numberofsugars);
+
+	});
+
+	
+	$("#lemon-form").submit(function(event) {
+		event.preventDefault();
+
+		var numberoflemons = $("#lemon-input").val();
+		Cookies.set("lemon-key", numberoflemons);
+
+	});
+
+
+	// $(".cookieimg").hover(function() {
+	// 	alert("Welcome! Please enter the number of each type of cookie you'd like in the boxes below and click Submit. If you change your mind click Reset to start over.");
+	// });
+
+// get the value
+// 	document.getElementById("sugarinput").value
 
 });
 
